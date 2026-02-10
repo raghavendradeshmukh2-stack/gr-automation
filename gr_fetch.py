6844b6f88ca22acb74be89890434579cd70e2611
raghavendradeshmukh2-stack <raghavendradeshmukh2@gmail.com>
2026-02-10 23:35:56 +0530

    Create gr_fetch.py

---
+++ gr_fetch.py
@@ -0,0 +1,27 @@
+mport requests
+from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Firebase Setup
+cred = credentials.Certificate("firebase_key.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': https://dynamic-gr-150b5-default-rtdb.asia-southeast1.firebasedatabase.app/
+})
+
+url = "https://gr.maharashtra.gov.in/1145/Government-Resolutions"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+gr_list = []
+
+for item in soup.select("table tr"):
+    text = item.get_text(strip=True)
+    if text:
+        gr_list.append(text)
+
+ref = db.reference("gr_data")
+ref.set(gr_list)
+
+print("GR Data Updated Successfully")

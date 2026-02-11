b4d353e1a3eeb33c2581a8b8db53de5b21da9212
raghavendradeshmukh2-stack <raghavendradeshmukh2@gmail.com>
2026-02-11 16:48:53 +0530

    Update gr_fetch.py

--- gr_fetch.py
+++ gr_fetch.py
@@ -4,28 +4,20 @@ import firebase_admin
 from firebase_admin import credentials, db
 import os
 import json
-
 # Firebase Setup
 firebase_key = json.loads(os.environ["FIREBASE_KEY"])
-
 cred = credentials.Certificate(firebase_key)
 firebase_admin.initialize_app(cred, {
     'databaseURL': "https://dynamic-gr-150b5-default-rtdb.asia-southeast1.firebasedatabase.app/"
 })
-
 url = "https://gr.maharashtra.gov.in/1145/Government-Resolutions"
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
-
 gr_list = []
-
 for item in soup.select("table tr"):
     text = item.get_text(strip=True)
     if text:
         gr_list.append(text)
-
 ref = db.reference("gr_data")
 ref.set(gr_list)
-
 print("GR Data Updated Successfully")
